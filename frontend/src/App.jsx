36e25593592b8f8a53d17a8a27f6d65dc8f8f637
adityajoshi14701 <adityajoshi14072001@google.com>
2025-05-16 11:50:16 +0530

    feat: add authentication flow with login, signup, and logout functionality; integrate react-hot-toast for notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,55 @@
-import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import React, { useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import SignupPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import Layout from "./layout/Layout";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
+import AddProblem from "./layout/AddProblem";
+import AdminRoute from "./components/AdminRoute";
+
 const App = () => {
-  let authUser = null;
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
     <div className="flex flex-col items-center justify-start">
+      <Toaster />
       <Routes>
-        <Route
-          path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
-        />
+        {/* // we can wrap this in a layout called auth layout and provide this functionality in that to make it more readable */}
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          />
+        </Route>
+
         <Route
           path="/login"
-          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
         />
         <Route
           path="/signup"
-          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+          element={!authUser ? <SignupPage /> : <Navigate to={"/"} />}
         />
+        <Route element={<AdminRoute />}>
+          <Route
+            path="/add-problem"
+            element={authUser ? <AddProblem /> : <Navigate to={"/"} />}
+          />
+        </Route>
       </Routes>
     </div>
   );
