265420e5dfa08384f8e77d311dfc5f600faf069d
adityajoshi14701 <adityajoshi14072001@google.com>
2025-06-02 23:41:37 +0530

    feat: implement admin route protection with loading state and redirection

--- frontend/src/components/AdminRoute.jsx
+++ frontend/src/components/AdminRoute.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
 const AdminRoute = () => {
-  return <div></div>;
+  const { authUser, isCheckingAuth } = useAuthStore();
+  if (isCheckingAuth) {
+    //change the loader from github file
+    return <div>Loading...</div>;
+  }
+
+  if (!authUser || authUser.role !== "admin") {
+    // If the user is not authenticated or not an admin, redirect to the login page
+    return <Navigate to="/login" replace />;
+  }
+  return <Outlet />; // Render the child components if the user is authenticated and an admin
 };
 
 export default AdminRoute;
